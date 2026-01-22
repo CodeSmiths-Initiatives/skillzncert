1acbae30458b3968de74bb7f77a9bde67c20219a
NiteshSukalikar <nsukalikar@innovastra.com>
2026-01-22 08:31:57 +0100

    added fixes for imgs

--- features/auth/Login.tsx
+++ features/auth/Login.tsx
@@ -100,11 +100,11 @@ export default function Login() {
   });
 
   return (
-    <AuthLayout imageSrc="/static/images/auth_image.png">
+    <AuthLayout imageSrc="static/images/auth_image.png">
       {/* HEADER */}
       <div className="text-center mb-6">
         <Image
-          src="/static/images/logo1.svg"
+          src="static/images/logo1.svg"
           alt="Logo"
           width={50}
           height={20}
