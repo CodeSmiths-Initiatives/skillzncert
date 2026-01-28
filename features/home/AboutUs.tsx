86af238286967a3e045671aa058bb2d4c04cd319
DevIdayah <oluwabukolairemide@gmail.com>
2026-01-28 17:51:52 +0100

    Fix the images paths and add montserrat font configuration

--- features/home/AboutUs.tsx
+++ features/home/AboutUs.tsx
@@ -83,7 +83,7 @@ export default function AboutUs() {
 							className="relative w-full h-65"
 						>
 							<Image
-								src="/static/images/aboutUs1.svg"
+								src="/static/images/Techexpert.jpg"
 								alt="Learning environment"
 								fill
 								className="object-cover rounded-3xl"
@@ -117,7 +117,7 @@ export default function AboutUs() {
 							className="absolute top-20 right-0 w-70 h-110"
 						>
 							<Image
-								src="/static/images/aboutUs1.svg"
+								src="/static/images/Techexpert.jpg"
 								alt="Learning environment"
 								fill
 								className="object-cover rounded-3xl"
