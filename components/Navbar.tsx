3172bdab2b81db9930a8182306d4926d9c2cab62
DevIdayah <oluwabukolairemide@gmail.com>
2026-01-29 15:39:55 +0100

    change the footer content and added more features to landing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,16 +15,17 @@ export default function Navbar() {
 					<Image
 						src="static/images/sec logo.svg"
 						alt="Logo"
-						width={50}
+						width={70}
 						height={10}
 					/>
-					<Image
-						src="static/images/skillsncert.svg"
-						alt="Logo"
-						width={150}
-						height={30}
-					/>
-					{/* <h3 className="text-xl font-semibold">CE-EMS</h3> */}
+					<Link href={"/"}>
+						<Image
+							src="static/images/skillsncert.svg"
+							alt="Logo"
+							width={200}
+							height={50}
+						/>
+					</Link>
 				</div>
 
 				<div className="hidden md:flex items-center space-x-10">
