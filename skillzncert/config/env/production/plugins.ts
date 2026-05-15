45e2112ebda6f9a930ae092b01544e5dda70c903
NiteshSukalikar <nsukalikar@innovastra.com>
2026-05-15 20:51:32 +0100

    added fixes for prod plugin

---
+++ skillzncert/config/env/production/plugins.ts
@@ -0,0 +1,20 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env.int("SMTP_PORT", 465),
+        secure: env.bool("SMTP_SECURE", true),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_FROM", env("SMTP_USERNAME")),
+        defaultReplyTo: env("SMTP_REPLY_TO", env("SMTP_USERNAME")),
+      },
+    },
+  },
+});
